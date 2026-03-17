bf82c6faf3ab3180b09186df84b02586a84ef8ca
gizemnurcrtn <cirtangizemnur@gmail.com>
2026-03-17 16:08:18 +0300

    feat: add basic flight controller

---
+++ Assets/FlightController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class FlightController : MonoBehaviour
+{
+    public float speed = 10f;
+    public float rotationSpeed = 60f;
+
+    void Update()
+    {
+        float move = 0f;
+        float turn = 0f;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            move = speed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            move = -speed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            turn = -rotationSpeed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            turn = rotationSpeed * Time.deltaTime;
+
+        transform.Translate(Vector3.forward * move);
+        transform.Rotate(Vector3.up * turn);
+    }
+}
